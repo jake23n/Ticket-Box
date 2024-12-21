b37ce688f16520c1258946d247f6e6ddbb87eac0
JakePham23 <jakeptnn@gmail.com>
2024-12-21 13:55:00 +0700

    change evironment db

--- src/dbs/init.mongodb.js
+++ src/dbs/init.mongodb.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose'
 import config from '../configs/enviroment.config.js';
 const dbConfig = config.db;
 
-let connectString = `mongodb+srv://${dbConfig.user}:${dbConfig.password}@${dbConfig.host}/${dbConfig.name}?retryWrites=true&w=majority`
+let connectString = `mongodb+srv://${dbConfig.user}:${dbConfig.password}@${dbConfig.host}/${dbConfig.name}?retryWrites=true&w=majority&appName=TicketBoxCluster`
 if(dbConfig.host === 'localhost'){
      connectString = 'mongodb://localhost:27017/TicketZEN'
 }
